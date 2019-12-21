d2a477d7081e833421d8d3fefbd99f2a449b5ded
Miwha Geschwind <miwhag@gmail.com>
2019-12-21 03:04:47 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,40 @@
 def addition(num1, num2)
-
+ puts num1 + num2
+ return num1 + num2
 end
+
 
 def subtraction(num1, num2)
-
+puts num1 - num2
+return num1-num2
 end
+
 
 def division(num1, num2)
-
+puts num1 / num2
+return num1 / num2
 end
+
 
 def multiplication(num1, num2)
-
+puts num1 * num2
+return num1*num2
 end
+
 
 def modulo(num1, num2)
-
+puts num1 % num2
+return num1 % num2
 end
 
-def square_root(num)
-
+def square_root (num)
+  puts Math.sqrt(num)
+  return Math.sqrt(num)
 end
+
+addition(1,2)
+subtraction(3,2)
+division(9,3)
+multiplication(4,2)
+modulo(9,4)
+square_root(3)
