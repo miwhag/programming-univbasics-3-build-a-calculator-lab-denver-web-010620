60f91c9cb767b0bbb37b5865e7fc636220388170
Miwha Geschwind <miwhag@gmail.com>
2019-12-21 02:56:11 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -8,22 +8,22 @@ puts num1 - num2
 end
 
 
-# def division(num1, num2)
-# puts num1 / num2
-#   returns num1 / num2
-# end
+def division(num1, num2)
+puts num1 / num2
+  returns num1 / num2
+end
 
 
-# def multiplication(num1, num2)
-# puts num1 * num2
-#   returns num1 * num2
-# end
+def multiplication(num1, num2)
+puts num1 * num2
+  returns num1 * num2
+end
 
 
-# def modulo(num1, num2)
-# puts num1 % num2
-#   returns num1 % num2
-# end
+def modulo(num1, num2)
+puts num1 % num2
+  returns num1 % num2
+end
 
 
 # def square_root(num)
